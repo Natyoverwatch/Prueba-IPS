0b5479a18a2e2ead32797eba952b4f25ebd45831
Natyoverwatch <natap455@gmail.com>
2023-04-26 00:24:01 -0500

    grupos de riesgo

--- src/Components/GruposDeRiego/index.js
+++ src/Components/GruposDeRiego/index.js
@@ -1,62 +1,169 @@
 import React from 'react'
 import "./style.scss"
 import { NavbarAdmin } from './../NavbarAdmin';
-import { useState } from 'react';
-import { Form, Row, Col, Button} from 'antd';
+import {  Row, Col, } from 'antd';
+import mamografia from '../../Images/mamografia.png';
+import sifilis from '../../Images/sifilis.png';
+import citologia from '../../Images/papilla.png';
+import desnutricion from '../../Images/desnutricion.png';
+import eda from '../../Images/diarrea.png';
+import ira from '../../Images/neumonia.png';
+import mme from '../../Images/muerte.png';
+import add from '../../Images/agregar-usuario.png';
+import { useNavigate } from 'react-router-dom';
+
+const data = [
+    {
+        id:0,
+        icon: mamografia,
+        Informacion:"Mamografía",
+        urlnavigate:"/",
+    },
+    {
+        id:1,
+        icon:sifilis, 
+        Informacion:"Sifilis gestacional y congenita",
+        urlnavigate:"/",
+        
+    },
+    {
+        id:2,
+        icon:citologia, 
+        Informacion:"Citologia y colposcopia",
+        urlnavigate:"/",
+        
+    },
+    {
+        id:3,
+        icon:desnutricion, 
+        Informacion:"Desnutricion",
+        urlnavigate:"/",
+    },
+    {
+        id:4,
+        icon:eda, 
+        Informacion:"EDA",
+        urlnavigate:"/",
+    },
+    {
+        id:5,
+        icon:ira, 
+        Informacion:"IRA",
+        urlnavigate:"/",
+    },
+    {
+        id:6,
+        icon: mme, 
+        Informacion:"MME",
+        urlnavigate:"/",
+    },
+    {
+        id:7,
+        icon:add, 
+        Informacion:"Agregar",
+        urlnavigate:"/",
+    }
+        ];
 
 
+export default function GruRiesgo(props){
 
-export default function GruRiesgo(){
-
-    const [datos, setDatos] = useState([]);
-    const [notes, setNotes] = useState([]);
-    let [counter, setCounter] = React.useState(0);
-    const [form] = Form.useForm();
-    //destructurng
-    //React.useState => [value, function to update the value];
-    const contador = (value) => {
-        setCounter(counter + value);
-    };
-
-    const agregar =() =>{
-        const newNote = {
-            name: datos,
-            id: counter,
-        }
-        setNotes([...notes, newNote])
-        setDatos('') //para que no siga anotando el ultimo valor
-    };
-
-    const remover =(id) =>{
-        const newNote = notes.filter((nota) => nota.id !== id)
-        setNotes(newNote)
-    };
-
-    const onReset = () => {
-        form.resetFields();
-    };
+    const navigate = useNavigate();
 
     return(
         <div>
             <NavbarAdmin/>
-            <div className='container'>
+            <div className='containerGR'>
                 <Row
                     className='styledRow'
                 >
-                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:6, offset:2 }} style={{}}>
-                    <h1>Citologia y colposcopia </h1>
-                    </Col>
-                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:6, offset:2 }} style={{}}>
-                    <h1>Desnutricion </h1>
-                    </Col>
-                    {notes.map((nota, index) => (
-                        <Col key={index} onClick={()=> remover(index)} className='boxAdd'>
-                            <p>{nota.name}</p>
+                    {
+                    data.map((read, index)=>(
+                        <Col 
+                            className='styledCol' 
+                            xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} 
+                            key={index}
+                            onClick={() => navigate(read.urlnavigate)}>
+                            <h1>{read.Informacion} </h1>
+                            <img src={read.icon} alt={read.Informacion}/>
                         </Col>
-                    ))}
+                    ))
+                }
                 </Row>
             </div>
         </div>
     )
 }
 
+/*import React from 'react'
+import "./style.scss"
+import { useState } from 'react';
+import {  Row, Col, } from 'antd';
+import mamografia from '../../Images/mamografia.png';
+import sifilis from '../../Images/sifilis.png';
+import citologia from '../../Images/papilla.png';
+import desnutricion from '../../Images/desnutricion.png';
+import eda from '../../Images/diarrea.png';
+import ira from '../../Images/neumonia.png';
+import mme from '../../Images/muerte.png';
+import add from '../../Images/agregar-usuario.png';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function GruRiesgo(props){
+
+    const navigate = useNavigate();
+    
+    return(
+        <div>
+            <NavbarAdmin/>
+            <div className='containerGR'>
+                <Row
+                    className='styledRow'
+                >
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>Mamografia </h1>
+                        <img src={mamografia} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>Sifilis gestacional y congenita </h1>
+                        <img src={sifilis} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>Citologia y colposcopia </h1>
+                        <img src={citologia} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>Desnutricion </h1>
+                        <img src={desnutricion} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>EDA </h1>
+                        <img src={eda} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>IRA </h1>
+                        <img src={ira} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>MME </h1>
+                        <img src={mme} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    <Col className='styledCol' xs={{span:20, offset:2 }} md={{span:10, offset:3 }} lg={{span:5, offset:2 }} style={{}}>
+                        <h1>Agregar </h1>
+                        <img src={add} alt="citologia" onClick={() => navigate('/')}/>
+                    </Col>
+                    {
+                    data.map((dataRead)=>(
+                            <Col
+                                key={dataRead.id}
+                                onClick={()=> setCount(dataRead.id)}>
+                            </Col>
+                    ))
+                }
+                </Row>
+            </div>
+        </div>
+    )
+}
+*/
--- src/Components/NavbarAdmin/index.js
+++ src/Components/NavbarAdmin/index.js
@@ -8,20 +8,20 @@ export const NavbarAdmin = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='NavbarContainerComponent'>
-            <div className='stylelogo'>
+        <div className='NavbarContainerComponentAdm'>
+            <div className='stylelogoAdm'>
                 <img src={logo} alt="logo" style={{ marginLeft: '4em' }} onClick={() => navigate('/')}/>
-                <p className='stylep'>SISALUD</p>
+                <p className='stylepAdm'>SISALUD</p>
             </div>
-            <div className='stylelink'>
+            <div className='stylelinkAdm'>
                 <Link to='/usuarios'>
-                    <h2>Gestion de usuario</h2>    
+                    <h2 className='styleh2Adm'>Gestion de usuario</h2>    
                 </Link>
                 <Link to='/griesgo'>
-                    <h2>Grupos de riesgo</h2>
+                    <h2 className='styleh2Adm'>Grupos de riesgo</h2>
                 </Link>
                 <Link to='/reportes'>
-                    <h2>Reportes</h2>
+                    <h2 className='styleh2Adm'>Reportes</h2>
                 </Link>
             </div>
             <div style={{ display: 'flex', marginRight: '4em', color: 'white' }}>
