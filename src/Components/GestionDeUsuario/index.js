1a7d66316139666d693b67435dffa108301c92bf
Santiago Jiménez <85375467+StormShadow010@users.noreply.github.com>
2023-04-26 19:25:05 -0500

    Gestión de Usuario
    Busqueda por usuario

--- src/Components/GestionDeUsuario/index.js
+++ src/Components/GestionDeUsuario/index.js
@@ -1,18 +1,64 @@
 import React, { useState } from 'react'
-import "../GestionDeUsuario/style.scss"
-import { Table, Button, Modal, Input } from 'antd';
+import { Table, Button, Modal, Input, Row, Col } from 'antd';
 import { FiEdit, FiTrash2 } from 'react-icons/fi';
-import { NavbarAdmin } from './../NavbarAdmin';
+import { SearchOutlined } from "@ant-design/icons"
+import "../GestionDeUsuario/style.scss"
+import { NavbarAdmin } from '../NavbarAdmin';
 
 export default function GestUser() {
     const [isEditing, setisEditing] = useState(false)
     const [isUserEditing, setisUserEditing] = useState('')
+
+    //Data people
+    const [dataSource, setDataSource] = useState([
+        {
+            key: '1',
+            nameuser: 'neto123',
+            password: 1234,
+            user: 'New York No. 1 Lake Park',
+            roll: 'admin',
+        },
+        {
+            key: '2',
+            nameuser: 'Jim Green',
+            password: 42,
+            user: 'London No. 1 Lake Park',
+            roll: 'auxiliar',
+        },
+        {
+            key: '3',
+            nameuser: 'Joe Black',
+            password: 32,
+            user: 'Sydney No. 1 Lake Park',
+            roll: 'usuario',
+        },
+    ])
+
     const columns = [
         {
             title: 'Nombre',
             dataIndex: 'nameuser',
             key: 'name',
             editable: true,
+            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => {
+                return (
+                    <>
+                        <Input
+                            autoFocus
+                            placeholder='Escribe aquí'
+                            onPressEnter={() => { confirm() }}
+                            onBlur={() => { confirm() }}
+                            value={selectedKeys[0]}
+                            onChange={(e) => {
+                                setSelectedKeys(e.target.value ? [e.target.value] : [])
+                                confirm({ closeDropdown: false })
+                            }}
+                        ></Input>
+                    </>
+                )
+            },
+            filterIcon: () => { return (<SearchOutlined />) },
+            onFilter: (value, record) => { return record.nameuser.toLowerCase().includes(value.toLowerCase()) },
         },
         {
             title: 'Contraseña',
@@ -47,33 +93,7 @@ export default function GestUser() {
         },
     ];
 
-    const [dataSource, setDataSource] = useState([
-        {
-            key: '1',
-            nameuser: 'neto123',
-            password: 1234,
-            user: 'New York No. 1 Lake Park',
-            roll: 'admin',
-            actions: ['edit', 'delete']
-        },
-        {
-            key: '2',
-            nameuser: 'Jim Green',
-            password: 42,
-            user: 'London No. 1 Lake Park',
-            roll: 'auxiliar',
-            actions: ['edit', 'delete']
-        },
-        {
-            key: '3',
-            nameuser: 'Joe Black',
-            password: 32,
-            user: 'Sydney No. 1 Lake Park',
-            roll: 'usuario',
-            actions: ['edit', 'delete']
-        },
-    ])
-
+    //Add new User 
     const onAddUser = () => {
         const newData = {
             key: '4',
@@ -81,21 +101,18 @@ export default function GestUser() {
             password: 32,
             user: 'Sydney No. 1 Lake Park',
             roll: 'usuario',
-            actions: ['edit', 'delete']
         };
         setDataSource([...dataSource, newData]);
     }
 
+    //Delete specific user
     const deleteUser = (key) => {
         console.log(key)
         const newData = dataSource.filter((userData) => userData.key !== key);
         setDataSource(newData);
-        return (
-            <>
-            </>
-        )
     }
 
+    //Update info from user
     const editUser = (key) => {
         console.log(key)
         setisEditing(true)
@@ -104,69 +121,48 @@ export default function GestUser() {
 
     return (
         <div>
-        <NavbarAdmin/>
-        <div className='containeruser'>
-            <div style={{ width: '100%' }}>
-                <Button style={{ marginBottom: '2rem' }} onClick={onAddUser}>Agregar un nuevo usuario</Button>
-                <Table columns={columns} dataSource={dataSource} />
-                <Modal
-                    title="Modificación"
-                    open={isEditing}
-                    cancelText='Cancelar'
-                    onCancel={() => { setisEditing(false); }}
-                    okText='Guardar'
-                    onOk={() => {
-                        setDataSource((pre) => {
-                            return pre.map((user) => {
-                                if (user.key === isUserEditing.key) {
-                                    return isUserEditing
-                                } else {
-                                    return user
-                                }
-                            })
-                        });
-                    }}
-                >
-                    <label>Nombre:</label>
-                    <Input value={isUserEditing?.nameuser}
-                        onChange={(e) => {
-                            setisUserEditing(pre => {
-                                return { ...pre, nameuser: e.target.value }
-                            }
-                            )
-                        }}
-                        
-                    ></Input>
-                    <label>Contraseña:</label>
-                    <Input value={isUserEditing?.password}
-                        onChange={(e) => {
-                            setisUserEditing(pre => {
-                                return { ...pre, password: e.target.value }
-                            }
-                            )
-                        }}
-
-                    ></Input>
-                    <label>Usuario:</label>
-                    <Input value={isUserEditing?.user}
-                        onChange={(e) => {
-                            setisUserEditing(pre => {
-                                return { ...pre, user: e.target.value }
-                            }
-                            )
-                        }}
-                    ></Input>
-                    <label>Roll:</label>
-                    <Input value={isUserEditing?.roll}
-                        onChange={(e) => {
-                            setisUserEditing(pre => {
-                                return { ...pre, roll: e.target.value }
-                            }
-                            )
-                        }}></Input>
-                </Modal>
-            </div >
-        </div >
+            <NavbarAdmin />
+            <div className='containeruser'>
+                <div style={{ width: '100%' }}>
+                    <Row style={{ display: 'flex', justifyContent: 'center', padding: '0 1em' }}>
+                        <Col className='tableUser'>
+                            <Button style={{ marginBottom: '2rem' }} onClick={onAddUser}>Agregar un nuevo usuario</Button>
+                            <Table columns={columns} dataSource={dataSource} />
+                            <Modal
+                                title="Modificación"
+                                open={isEditing}
+                                cancelText='Cancelar'
+                                onCancel={() => { setisEditing(false); }}
+                                okText='Guardar'
+                                onOk={() => {
+                                    setDataSource((pre) => {
+                                        return pre.map((user) => { return user.key === isUserEditing.key ? isUserEditing : user })
+                                    });
+                                    setisEditing(false);
+                                }}
+                            >
+                                <label>Nombre:</label>
+                                <Input value={isUserEditing?.nameuser}
+                                    onChange={(e) => { setisUserEditing(pre => { return { ...pre, nameuser: e.target.value } }) }}
+                                ></Input>
+                                <label>Contraseña:</label>
+                                <Input value={isUserEditing?.password}
+                                    onChange={(e) => { setisUserEditing(pre => { return { ...pre, password: e.target.value } }) }}
+                                ></Input>
+                                <label>Usuario:</label>
+                                <Input value={isUserEditing?.user}
+                                    onChange={(e) => { setisUserEditing(pre => { return { ...pre, user: e.target.value } }) }}
+                                ></Input>
+                                <label>Roll:</label>
+                                <Input value={isUserEditing?.roll}
+                                    onChange={(e) => { setisUserEditing(pre => { return { ...pre, roll: e.target.value } }) }}
+                                ></Input>
+                            </Modal>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
         </div>
     )
 }
+
