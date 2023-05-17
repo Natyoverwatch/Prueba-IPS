fb640b8de4c95e43145cb07430713bcb3176f715
Santiago Jiménez <85375467+StormShadow010@users.noreply.github.com>
2023-05-16 21:44:54 -0500

    Riesgo grupos
    Modificación Grupos de riesgo

--- src/Components/GruposDeRiego/index.js
+++ src/Components/GruposDeRiego/index.js
@@ -1,96 +1,119 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import "./style.scss"
 import { NavbarAdmin } from './../NavbarAdmin';
-import { Row, Col, } from 'antd';
-import mamografia from '../../Images/mamografia.png';
-import sifilis from '../../Images/sifilis.png';
-import citologia from '../../Images/papilla.png';
-import desnutricion from '../../Images/desnutricion.png';
-import eda from '../../Images/diarrea.png';
-import ira from '../../Images/neumonia.png';
-import mme from '../../Images/muerte.png';
-import add from '../../Images/agregar-usuario.png';
 import { useNavigate } from 'react-router-dom';
+import { Form, Modal, Button, Input, Row, Col } from 'antd';
+import { addData, getData, editData, deleteData } from "../../controller/control"
+import { AppContext } from '../../Provider';
 
-const data = [
-    {
-        id: 0,
-        icon: mamografia,
-        Informacion: "Mamografía",
-        urlnavigate: "/mamografia",
-    },
-    {
-        id: 1,
-        icon: sifilis,
-        Informacion: "Sifilis gestacional y congenita",
-        urlnavigate: "/sifilis",
+//Images
+import mamografiaImage from '../../Images/mamografia.png';
+import sifilisImage from '../../Images/sifilis.png';
+import citologiaImage from '../../Images/papilla.png';
+import desnutricionImage from '../../Images/desnutricion.png';
+import edaImage from '../../Images/diarrea.png';
+import iraImage from '../../Images/neumonia.png';
+import mmeImage from '../../Images/muerte.png';
+import addImage from '../../Images/agregar-usuario.png';
 
-    },
-    {
-        id: 2,
-        icon: citologia,
-        Informacion: "Citologia y colposcopia",
-        urlnavigate: "/citologia",
 
-    },
-    {
-        id: 3,
-        icon: desnutricion,
-        Informacion: "Desnutricion",
-        urlnavigate: "/desnutricion",
-    },
-    {
-        id: 4,
-        icon: eda,
-        Informacion: "EDA",
-        urlnavigate: "/eda",
-    },
-    {
-        id: 5,
-        icon: ira,
-        Informacion: "IRA",
-        urlnavigate: "/ira",
-    },
-    {
-        id: 6,
-        icon: mme,
-        Informacion: "MME",
-        urlnavigate: "/mme",
-    },
-    {
-        id: 7,
-        icon: add,
-        Informacion: "Agregar",
-        urlnavigate: "/",
+export default function GruRiesgo() {
+
+    const riksImages = {
+        mamografía: mamografiaImage,
+        sifilisgestacionalycongenita: sifilisImage,
+        citologia: citologiaImage,
+        desnutrición: desnutricionImage,
+        eda: edaImage,
+        ira: iraImage,
+        mme: mmeImage,
+        add: addImage,
     }
-];
 
+    const filteredRisk = (riskGroup) => {
+        const foundPair = Object.entries(riksImages).find(([key, value]) => key === riskGroup.replace(/\s+/g, ''));
+        if (foundPair) {
+            const [key, value] = foundPair;
+            return value
+        } else {
+            console.log('Pair not found.');
+        }
+    }
 
-export default function GruRiesgo(props) {
+    //Global state
+    const [state, setState] = useContext(AppContext)
 
     const navigate = useNavigate();
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const [form] = Form.useForm();
+
+    //Data risk groups
+    const [dataSource, setDataSource] = useState([]);
+
+    //Create new risk 
+    const createNewRisk = async (values) => {
+        setIsModalVisible(false);
+        const data = await addData(values, "https://api.clubdeviajeros.tk/api/risk", state?.token)
+        console.log(data)
+    };
+
+    const getRisks = async () => {
+        const getConstdata = await getData("https://api.clubdeviajeros.tk/api/risk", state?.token)
+        console.log(getConstdata)
+        setDataSource(getConstdata);
+    }
+
+    useEffect(() => {
+        getRisks()
+        // eslint-disable-next-line
+    }, [])
 
     return (
         <div>
             <NavbarAdmin />
-            <div className='containerGR'>
-                <Row
-                    className='styledRow'
-                >
-                    {
-                        data.map((read, index) => (
-                            <Col
-                                className='styledCol'
-                                xs={{ span: 20, offset: 2 }} md={{ span: 10, offset: 3 }} lg={{ span: 5, offset: 2 }}
-                                key={index}
-                                onClick={() => navigate(read.urlnavigate)}>
-                                <h1>{read.Informacion} </h1>
-                                <img src={read.icon} alt={read.Informacion} />
-                            </Col>
-                        ))
-                    }
-                </Row>
-            </div>
+            <Button type="primary" onClick={() => { setIsModalVisible(true) }}>
+                Abrir Modal
+            </Button>
+            <Modal
+                title="Ejemplo de Modal con Formulario"
+                open={isModalVisible}
+                onCancel={() => setIsModalVisible(false)}
+                footer={[
+                    <Button key="cancel" onClick={() => setIsModalVisible(false)}>
+                        Cancelar
+                    </Button>,
+                    <Button key="create" type="primary" onClick={() => { form.submit() }}>
+                        Crear
+                    </Button>,
+                ]}
+            >
+                <Form form={form} onFinish={createNewRisk}>
+                    <Form.Item
+                        name="name"
+                        label="Nombre"
+                        rules={[{ required: true, message: 'Por favor ingresa un nombre' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </Modal>
+            <Row className='styledRow'>
+                {
+                    dataSource.map((read, index) => (
+                        <Col
+                            className='styledCol'
+                            xs={{ span: 20, offset: 2 }} md={{ span: 10, offset: 3 }} lg={{ span: 5, offset: 2 }}
+                            key={index}
+                            onClick={() => navigate(read.urlnavigate)}>
+                            <h1 style={{ textTransform: "capitalize" }}>{read.name} </h1>
+                            <img src={filteredRisk(read.name)} alt={read.name + "imagen"} />
+                        </Col>
+                    ))
+                }
+            </Row>
         </div>
     )
 }
+
